2f8a4edad45ab5856b8148a00d229bfd864a45f2
Mostafa Elhoushi <melhoushi@meta.com>
2024-10-17 17:25:39 -0400

    Support llama3.2 1b (#8)
    * update transformers version
    
    * unclean code that worked
    
    * remove unnecessary functions
    
    * clean up code
    
    * more cleanup
    
    * more clean up

--- self_speculation/llama_model_utils.py
+++ self_speculation/llama_model_utils.py
@@ -9,9 +9,7 @@ from dataclasses import dataclass
 from typing import List, Optional, Tuple
 
 import torch
-
 import transformers
-
 
 @dataclass
 class ForwardResult:
@@ -19,6 +17,94 @@ class ForwardResult:
     past_key_values: Optional[List[Tuple[torch.Tensor, torch.Tensor]]]
     exit_query_cache: Optional[List[torch.Tensor]]
 
+# Copied from transformers.models.bart.modeling_bart.BartDecoder._prepare_decoder_attention_mask
+def _prepare_decoder_attention_mask(model, attention_mask, input_shape, inputs_embeds, past_key_values_length):
+    # create causal mask
+    # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+    combined_attention_mask = None
+    if input_shape[-1] > 1:
+        combined_attention_mask = _make_causal_mask(
+            input_shape,
+            inputs_embeds.dtype,
+            device=inputs_embeds.device,
+            past_key_values_length=past_key_values_length,
+        )
+
+    if attention_mask is not None:
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
+            inputs_embeds.device
+        )
+        combined_attention_mask = (
+            expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
+        )
+
+    return combined_attention_mask
+
+# Copied from transformers.models.bart.modeling_bart._make_causal_mask
+def _make_causal_mask(
+    input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
+):
+    """
+    Make causal mask used for bi-directional self-attention.
+    """
+    bsz, tgt_len = input_ids_shape
+    mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+    mask_cond = torch.arange(mask.size(-1), device=device)
+    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+    mask = mask.to(dtype)
+
+    if past_key_values_length > 0:
+        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
+    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
+
+# Copied from transformers.models.bart.modeling_bart._expand_mask
+def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
+    """
+    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
+    """
+    bsz, src_len = mask.size()
+    tgt_len = tgt_len if tgt_len is not None else src_len
+
+    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+
+    inverted_mask = 1.0 - expanded_mask
+
+    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+
+def top_k_top_p_filtering(
+    logits: torch.FloatTensor,
+    top_k: int = 0,
+    top_p: float = 1.0,
+    filter_value: float = -float("Inf"),
+    min_tokens_to_keep: int = 1,
+) -> torch.FloatTensor:
+    """
+    Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+
+    Args:
+        logits: logits distribution shape (batch size, vocabulary size)
+        top_k (`int`, *optional*, defaults to 0):
+            If > 0, only keep the top k tokens with highest probability (top-k filtering)
+        top_p (`float`, *optional*, defaults to 1.0):
+            If < 1.0, only keep the top tokens with cumulative probability >= top_p (nucleus filtering). Nucleus
+            filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+        min_tokens_to_keep (`int`, *optional*, defaults to 1):
+            Minimumber of tokens we keep per batch example in the output.
+
+    From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+    if top_k > 0:
+        logits = transformers.generation.logits_process.TopKLogitsWarper(top_k=top_k, filter_value=filter_value, min_tokens_to_keep=min_tokens_to_keep)(
+            None, logits
+        )
+
+    if 0 <= top_p <= 1.0:
+        logits = transformers.generation.logits_process.TopPLogitsWarper(top_p=top_p, filter_value=filter_value, min_tokens_to_keep=min_tokens_to_keep)(
+            None, logits
+        )
+
+    return logits
 
 def decode_next_token(
     logits: torch.Tensor,
@@ -37,7 +123,7 @@ def decode_next_token(
     else:
         if not token_idx:
             logits.squeeze_(dim=0)
-        filtered_logits = transformers.top_k_top_p_filtering(logits / temperature, top_k=top_k, top_p=top_p)
+        filtered_logits = top_k_top_p_filtering(logits / temperature, top_k=top_k, top_p=top_p)
         probabilities = torch.nn.functional.softmax(filtered_logits, dim=-1)
         next_token = torch.multinomial(probabilities, num_samples=1)
         if not token_idx:
@@ -51,6 +137,8 @@ def crop_past_key_values(
 ) -> List[Tuple[torch.Tensor, torch.Tensor]]:
     new_past: List[Tuple[torch.Tensor, torch.Tensor]] = []
     for idx in range(len(past_key_values)):
+        if past_key_values[idx] is None or past_key_values[idx][0] == [] or past_key_values[idx][0] is None:
+            break
         new_past.append(
             (
                 past_key_values[idx][0][:, :, :maximum_length, :],
@@ -77,6 +165,8 @@ def forward_early(
     if past_key_values is not None:
         past_key_values_length = past_key_values[0][0].shape[2]
         seq_length_with_past = seq_length_with_past + past_key_values_length
+    past_key_values = transformers.cache_utils.DynamicCache.from_legacy_cache(past_key_values)
+
     position_ids = torch.arange(
         past_key_values_length,
         seq_length + past_key_values_length,
@@ -89,32 +179,29 @@ def forward_early(
         dtype=torch.bool,
     )
     inputs_embeds = model.model.embed_tokens(input_ids)
-    attention_mask = model.model._prepare_decoder_attention_mask(
+    attention_mask = _prepare_decoder_attention_mask(
+        model,
         attention_mask,
         (batch_size, seq_length),
         inputs_embeds,
         past_key_values_length,
     )
 
-    next_decoder_cache = []
     hidden_states = inputs_embeds
-    for idx, decoder_layer in enumerate(model.model.layers[:exit_layer]):
-        past_key_value = past_key_values[idx] if past_key_values is not None else None
-        layer_outputs = decoder_layer(
+    for decoder_layer in model.model.layers[:exit_layer]:
+        hidden_states, past_key_values = decoder_layer(
             hidden_states,
             attention_mask=attention_mask,
             position_ids=position_ids,
-            past_key_value=past_key_value,
+            past_key_value=past_key_values,
             output_attentions=False,
             use_cache=True,
             padding_mask=None,
         )
 
-        hidden_states = layer_outputs[0]
+    past_key_values = past_key_values.to_legacy_cache()
 
-        next_decoder_cache.append(layer_outputs[1])
-
-    next_cache = next_decoder_cache
+    # next_cache = next_decoder_cache
     if exit_query_cache is None:
         exit_query_cache = hidden_states
     else:
@@ -122,13 +209,9 @@ def forward_early(
 
     hidden_states = model.model.norm(hidden_states)
 
-    # add any layers that were not used into the KV cache for reuse
-    if past_key_values is not None:
-        next_cache.extend(past_key_values[len(next_cache) :])
-
     logits = model.lm_head(hidden_states)
     return ForwardResult(
-        logits=logits, past_key_values=next_cache, exit_query_cache=exit_query_cache
+        logits=logits, past_key_values=past_key_values, exit_query_cache=exit_query_cache
     )
 
 
@@ -146,7 +229,7 @@ def forward_remainder(
     draft_past_key_values_length: int = 0
     full_past_key_values_length: int = 0
 
-    if past_key_values is not None:
+    if past_key_values is not None and past_key_values[0] is not None:
         # it's okay to use the first layer because the draft model necessairly computes it
         draft_past_key_values_length = past_key_values[0][0].shape[2]
         # the total sequence length is the past key values since that includes the draft tokens
@@ -160,6 +243,7 @@ def forward_remainder(
             full_past_key_values_length = 0
 
         seq_length_with_past = num_tokens_to_generate + draft_past_key_values_length
+    past_key_values = transformers.cache_utils.DynamicCache.from_legacy_cache(past_key_values)
 
     inputs_embeds = model.model.embed_tokens(input_ids)
 
@@ -174,14 +258,16 @@ def forward_remainder(
         (batch_size, seq_length_with_past),
         dtype=torch.bool,
     )
-    early_attention_mask = model.model._prepare_decoder_attention_mask(
+    early_attention_mask = _prepare_decoder_attention_mask(
+        model,
         attention_mask,
         (batch_size, num_tokens_to_generate),
         inputs_embeds,
         draft_past_key_values_length,
     )
 
-    full_attention_mask = model.model._prepare_decoder_attention_mask(
+    full_attention_mask = _prepare_decoder_attention_mask(
+        model,
         attention_mask,
         (batch_size, seq_length),
         inputs_embeds,
@@ -203,11 +289,11 @@ def forward_remainder(
             # early hidden states: B x num_gen x C
             early_hidden_states = hidden_states[:, -num_tokens_to_generate:]
             early_position_ids = position_ids[:, -num_tokens_to_generate:]
-            layer_outputs = decoder_layer(
+            hidden_states, past_key_values = decoder_layer(
                 early_hidden_states,
                 attention_mask=early_attention_mask,
                 position_ids=early_position_ids,
-                past_key_value=past_key_value,
+                past_key_value=past_key_values,
                 output_attentions=False,
                 use_cache=True,
                 padding_mask=None,
@@ -224,22 +310,20 @@ def forward_remainder(
             else:
                 # we already have seen the fully hidden states we can re-use them now
                 full_hidden_states = hidden_states
-            layer_outputs = decoder_layer(
+            hidden_states, past_key_values = decoder_layer(
                 full_hidden_states,
                 attention_mask=full_attention_mask,
                 position_ids=position_ids,
-                past_key_value=past_key_value,
+                past_key_value=past_key_values,
                 output_attentions=False,
                 use_cache=True,
                 padding_mask=None,
             )
-        hidden_states = layer_outputs[0]
-        next_decoder_cache.append(layer_outputs[1])
 
+    past_key_values = past_key_values.to_legacy_cache()
     hidden_states = model.model.norm(hidden_states)
     logits = model.lm_head(hidden_states)
 
-    next_cache = next_decoder_cache
     return ForwardResult(
-        logits=logits, past_key_values=next_cache, exit_query_cache=exit_query_cache
+        logits=logits, past_key_values=past_key_values, exit_query_cache=exit_query_cache
     )
